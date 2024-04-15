e903f323e979d967dbc9d813dd3385d06ccb923c
V-Mokhun <mohunmax1408@gmail.com>
2024-04-15 11:25:04 +0200

    Add: shorten url and insert into db

--- urlshortener/main.go
+++ urlshortener/main.go
@@ -13,9 +13,28 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type DBUrl struct {
+	key       string
+	short_url string
+	long_url  string
+}
+
+const KEY_LENGTH int = 8
+
 var db *sql.DB
 
 func main() {
+	connectToDB()
+	defer db.Close()
+
+	res, err := shortenUrl("https://www.google.com/search?q=hash&oq=something#something")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res)
+}
+
+func connectToDB() {
 	cfg := mysql.Config{
 		User:   os.Getenv("DBUSER"),
 		Passwd: os.Getenv("DBPASS"),
@@ -28,14 +47,54 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
 	pingErr := db.Ping()
 	if pingErr != nil {
 		log.Fatal(pingErr)
 	}
+}
 
-	fmt.Println(hashUrl("https://www.google.com/search?q=hash&oq=something#something"))
+func queryUrl(key string) (DBUrl, error) {
+	var url DBUrl
+
+	row := db.QueryRow("SELECT * FROM url WHERE `key` = ?", key)
+	if err := row.Scan(&url.key, &url.long_url, &url.short_url); err != nil {
+		if err == sql.ErrNoRows {
+			return url, sql.ErrNoRows
+		}
+		return url, fmt.Errorf("unexpected error")
+	}
+
+	return url, nil
+}
+
+func shortenUrl(urlToShorten string) (string, error) {
+	key := hashUrl(urlToShorten)
+	shortenedKey := key[:KEY_LENGTH]
+	_, err := queryUrl(shortenedKey)
+
+	// if key does not exist in db
+	if err != nil {
+		if err == sql.ErrNoRows {
+			_, insertErr := db.Exec("INSERT INTO url (`key`, long_url, short_url) VALUES(?, ?, ?)", shortenedKey, urlToShorten, "http://localhost/"+shortenedKey)
+
+			// TODO: handle insertion error http
+			if insertErr != nil {
+				return shortenedKey, insertErr
+			}
+
+			fmt.Println("inserted key: " + shortenedKey)
+
+			// TODO: handle http 200 status
+			return shortenedKey, nil
+		} else {
+			// TODO: handle http status when error
+			return shortenedKey, err
+		}
+	}
+
+	// TODO: handle url already exists
+	return shortenedKey, nil
 }
 
 func hashUrl(urlToHash string) string {
